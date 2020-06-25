4fef7441d72b88112dc6f1a645a358bd7500b478
Tongzi <tongziwoo001@gmail.com>
2020-06-26 00:22:05 +0800

    version 0.15 by tz, added checkout procedure

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
 import React from 'react';
 import './App.css';
 import './style/html5up-dimension/assets/css/fontawesome-all.min.css';
-const ipaddr = 'http://39.106.86.23:8000'
+const ipaddr = 'http://39.106.86.23:8000'   // 后端服务器地址
+const default_setting = {
+  interval_heartbeat: 5000,         // 发送心跳包的频率
+  interval_update_room_temp: 5000,  // 进行室内温度更新的频率
+  is_checked_in: false,             // 是否已经入住
+  is_on: false,                     // 空调是否已经开启
+  is_served: false,                 // 是否正在送风
+  room_id: "000",                   // 空调（房间）编号
+  room_temp: 0.0,                   // 房间温度
+  default_room_temp: 0.0,           // 默认房间温度
+  ac_mode: 0,                       // 制热/制冷
+  ac_wind: 2,                       // 风速
+  ac_actual_wind: 0,                // 此时实际被送的风速
+  ac_temp: 25,                      // 设定的空调温度
+  temp_min: 0,                      // 空调温度范围
+  temp_max: 40,
+  online_time: 0,                   // 在线时间，属于后端处理数据，下同
+  checkin_time: "",                 // 入住时间
+  power: 0,                         // 已使用电量
+  expense: 0,                       // 已花费钱数
+  charge_policy: 0                  // 每度电花费
+}
 class App extends React.Component {
   constructor(props){
     super(props);
-    this.state = {
-      interval_heartbeat: 5000,
-      interval_update_room_temp: 5000,
-      is_checked_in: false,
-      is_on: false,
-      is_served: false,
-      room_id: "000",
-      room_temp: 0.0,
-      default_room_temp: 0.0,
-      ac_mode: 0,
-      ac_wind: 1,
-      ac_actual_wind: 1,
-      ac_temp: 26,
-      temp_min: 0,
-      temp_max: 40,
-      online_time: 0,
-      checkin_time: "",
-      power: 0,
-      expense: 0,
-      charge_policy: 0
-    };
+    this.state = default_setting;
   }
 
-  wind_int2str(wind) {
+  wind_int2str(wind) {    // 将前端的数字类型风速转换为后端字符串类型
     switch (wind) {
       case 1: 
         return "low";
@@ -42,7 +43,7 @@ class App extends React.Component {
     }
   }
 
-  wind_str2int(wind) {
+  wind_str2int(wind) {    // 将后端的字符串类型风速转换为前端数字类型
     switch (wind) {
       case "low": 
         return 1;
@@ -56,56 +57,71 @@ class App extends React.Component {
     }
   }
 
-  parse_res(res) {
+  test_error(res) {     // 测试返回内容中是否有报错字段
     let json = JSON.parse(res.text);
     if (json.hasOwnProperty('Error')) {
-      alert(json['Error'])
-    } else {
-      let room_id = json['room_id'];
-      let ac_status = json['ac_status'];
-      let ac_actual_wind = this.state.ac_wind;
-      let is_served = this.state.is_served;
-      if (ac_status !== 'off') {
-        is_served = true;
-        ac_actual_wind = this.wind_str2int(ac_status);
-      }
-      else {
-        is_served = false;
-      }
-      let power = json['elec'];
-      let online_time = json['online_time'];
-      let checkin_time = json['checkin_time'];
-      let is_checked_in = json['checked'];
-      let expense = json['total_money'];
-      let charge_policy = json['price'];
-      this.setState({
-        is_checked_in: is_checked_in,
-        is_served: is_served,
-        room_id: room_id,
-        ac_actual_wind: ac_actual_wind,
-        online_time: online_time,
-        checkin_time: checkin_time,
-        power: power,
-        expense: expense,
-        charge_policy: charge_policy
-      });
+      alert(json['Error']);
+      return -1;
     }
-    
+    else{
+      return 0;
+    }
+  }
+
+  parse_res(res) {      // 通用的数据返回解析函数
+    let json = JSON.parse(res.text);
+    let room_id = json['room_id'];
+    let ac_status = json['ac_status'];
+    let ac_actual_wind = this.state.ac_wind;
+    let is_served = this.state.is_served;
+    if (ac_status !== 'off') {  // 若风速不是0，说明正在送风
+      is_served = true;
+      ac_actual_wind = this.wind_str2int(ac_status);
+    }
+    else {  // 没有送风
+      is_served = false;
+    }
+    let power = json['elec'];
+    let online_time = json['online_time'];
+    let checkin_time = json['checkin_time'];
+    let is_checked_in = json['checked'];
+    if (!is_checked_in) {   // 若此时已经checkout，需要暂停heartbeat的定时调用
+      this.set_checkout();
+    }
+    let expense = json['total_money'];
+    let charge_policy = json['price'];
+    this.setState({
+      is_checked_in: is_checked_in,
+      is_served: is_served,
+      room_id: room_id,
+      ac_actual_wind: ac_actual_wind,
+      online_time: online_time,
+      checkin_time: checkin_time,
+      power: power,
+      expense: expense,
+      charge_policy: charge_policy
+    });   
+  }
+
+  set_checkout() {
+    this.interval_heartbeat && clearInterval(this.interval_heartbeat);
+    alert('You have already checked out. The system will soon exit.');
+    this.setState(default_setting);
   }
   
-  componentWillUnmount() {
-    clearInterval(this.interval_heartbeat);
+  componentWillUnmount() {          // 页面卸载时将计时器清除
+    this.interval_heartbeat && clearInterval(this.interval_heartbeat);
   }
 
-  start_order_timer(wind, temp) {
+  start_order_timer(wind, temp) {   // 开始本次调温指令的1s计时
     this.interval_order = setInterval(() => this.set_ac_mode(wind, temp), 1000);
   }
 
-  stop_order_timer() {
+  stop_order_timer() {              // 结束本次计时
     this.interval_order && clearInterval(this.interval_order);
   }
 
-  update_ac_mode(ac_temp) {
+  update_ac_mode(ac_temp) {         // 更新制冷/制热模式
     let ac_mode = 0;
     ac_mode = ac_temp > 25 ? 1 : 0;
     this.setState({
@@ -114,10 +130,10 @@ class App extends React.Component {
     //console.log("ac_temp: " + ac_temp + " ac_mode: " + ac_mode)
   }
 
-  update_room_temp() {
+  update_room_temp() {              // 更新房间实时温度
     let room_temp = this.state.room_temp;
     let int = this.state.interval_update_room_temp;
-    if (this.state.is_served) {
+    if (this.state.is_served) {     // 送风情况下
       if (Number(this.state.room_temp).toFixed(1) < this.state.ac_temp) {
         room_temp += (0.5 + (this.state.ac_actual_wind - 2)*0.2) / (60000 / int)
       } 
@@ -130,7 +146,7 @@ class App extends React.Component {
       })
       console.log(this.state.room_temp)
     }
-    else {
+    else {                          // 没有送风，恢复到默认温度
       if (Number(this.state.room_temp).toFixed(1) < this.state.default_room_temp) {
         room_temp += 0.5 / (60000 / int)
       } 
@@ -144,7 +160,7 @@ class App extends React.Component {
     }
   }
 
-  heartbeat() {
+  heartbeat() {   // 心跳包，监测服务器情况，同时更新环境温度到数据库
     this.update_room_temp();
     var request = require('superagent');
     request
@@ -152,40 +168,47 @@ class App extends React.Component {
       .send({"room_id": this.state.room_id, 
             "temp": Number(this.state.room_temp).toFixed(1)})
       .then(res => {
-        this.parse_res(res);
+        if (this.test_error(res) === 0) {
+          this.parse_res(res);
+        }
       })
       .catch (err => {
         alert(err);
       });
   }
 
-  check_in() {
+  check_in() {    // 入住酒店，将分配房号和部分初值
     var request = require('superagent');
     request
       .get(ipaddr + '/api/user/checkin/')
       .then(res => {
-        this.parse_res(res);
-        let json = JSON.parse(res.text);
-        let temp_min = json['temp_min'];
-        let temp_max = json['temp_max'];
-        let room_temp = json['temp'];
-        this.update_ac_mode(room_temp);
-        let default_room_temp = room_temp;
-        this.setState({
-          temp_min: temp_min,
-          temp_max: temp_max,
-          room_temp: room_temp,
-          default_room_temp: default_room_temp
-          }); 
-          this.interval_heartbeat = setInterval(() => this.heartbeat(), this.state.interval_heartbeat);
-          console.log(this.state)
+        if (this.test_error(res) === 0)
+        {
+          this.parse_res(res);
+          let json = JSON.parse(res.text);
+          let temp_min = Number(json['temp_min']);
+          let temp_max = Number(json['temp_max']);
+          let room_temp = Number(json['temp']);
+          let ac_temp = Number(json['target_temp']);
+          this.update_ac_mode(room_temp);
+          let default_room_temp = room_temp;
+          this.setState({
+            temp_min: temp_min,
+            temp_max: temp_max,
+            room_temp: room_temp,
+            ac_temp: ac_temp,
+            default_room_temp: default_room_temp
+            }); 
+            this.interval_heartbeat = setInterval(() => this.heartbeat(), this.state.interval_heartbeat);
+        }
+          console.log(this.state);
       })
       .catch (err => {
-        alert(err)
+        alert(err);
       })
   }
 
-  power_on_off() {
+  power_on_off() {    // 开机/关机
     var request = require('superagent');
     request
       .post(ipaddr + '/api/user/setmode/')
@@ -193,19 +216,21 @@ class App extends React.Component {
             "ac_status": this.state.is_on ? "off" : this.wind_int2str(this.state.ac_wind), 
             "target_temp": Number(this.state.ac_temp).toFixed(1)})
       .then(res => {
-        this.parse_res(res);
-        let is_on = !this.state.is_on;
-        this.setState({
-          is_on: is_on
-        })
-        console.log(this.state)
+        if (this.test_error(res) === 0) {
+          this.parse_res(res);
+          let is_on = !this.state.is_on;
+          this.setState({
+            is_on: is_on
+          });
+        }
+        console.log(this.state);
       })
       .catch (err => {
         alert(err)
       })
   }
 
-  set_ac_mode(wind, temp){
+  set_ac_mode(wind, temp){  // 设置空调风速或温度
     var request = require('superagent')
     request
       .post(ipaddr + '/api/user/setmode/')
@@ -214,6 +239,7 @@ class App extends React.Component {
             "target_temp": Number(temp).toFixed(0)})
       .then(res => {
         this.stop_order_timer();
+        this.test_error(res);
         console.log(this.state)
       })
       .catch (err => {
@@ -221,7 +247,7 @@ class App extends React.Component {
       })
   }
 
-  set_temp(temp) {
+  set_temp(temp) {    // 设置空调温度
     if (temp < this.state.temp_min || temp > this.state.temp_max) {
       alert('Out of temp range!');
       return;
@@ -234,7 +260,7 @@ class App extends React.Component {
     this.start_order_timer(this.wind_int2str(this.state.ac_wind), temp);
   }
 
-  set_wind(wind) {
+  set_wind(wind) {    // 设置空调风速
     if (wind <= 0 || wind > 3) {
       alert('Out of wind range!');
       return;
